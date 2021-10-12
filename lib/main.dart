42c44e1d6d3a8d60e098f350037b9aa20b772757
wuhaohao1234 <1611499758@qq.com>
2021-10-12 23:26:08 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('helllo'),
+          backgroundColor: const Color(0x00000000),
+        ),
+        body: const Text('Hello'),
+      ),
+    );
+  }
+}
